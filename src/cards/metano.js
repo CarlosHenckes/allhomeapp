bc4f036876033a04d2490408e6750f132bf6778e
Carlos Henckes <henckes@uol.com.br>
2021-10-30 21:04:07 -0300

    Add files via upload

--- src/cards/metano.js
+++ src/cards/metano.js
@@ -5,10 +5,10 @@ const MethaneCard = (t) => {
     return (
         <div className="databox col-sm-6">
 
-                <div className="data size30" id="cardvalue">{t.value}%</div>
+                <div className="data size30" id="cardvalue">{t.value}ppm</div>
                 <div className="dataescaled" id="cardeq">&nbsp;</div>
             <div className="icon">
-            <img id="cardimage" alt="Metano" title="Metano" src="./methane.png" height="70px" className="ico_metano" />
+            <img id="cardimage" alt="Metano" title="Metano" src="./methane.png" height="70px" className="ico_mgitetano" />
             </div>
             <div className="label" id="cardlabel">METANO</div>
         </div>
