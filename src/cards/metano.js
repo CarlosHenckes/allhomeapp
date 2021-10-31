1124e72a0b396fcd4ff78927c487efc2789b8a1a
Carlos Henckes <henckes@uol.com.br>
2021-10-30 21:03:36 -0300

    Add files via upload

---
+++ src/cards/metano.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const MethaneCard = (t) => {
+
+    return (
+        <div className="databox col-sm-6">
+
+                <div className="data size30" id="cardvalue">{t.value}%</div>
+                <div className="dataescaled" id="cardeq">&nbsp;</div>
+            <div className="icon">
+            <img id="cardimage" alt="Metano" title="Metano" src="./methane.png" height="70px" className="ico_metano" />
+            </div>
+            <div className="label" id="cardlabel">METANO</div>
+        </div>
+    )
+}
+export default MethaneCard;
